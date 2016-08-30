c2483d9f87377807312c25f1aae8a34c9bc6648a
kmd1998gh <kmd1998.wr@gmail.com>
2016-08-30 19:05:47 +0900

    Update preview.js

--- preview.js
+++ preview.js
@@ -1,19 +1,19 @@
 $(function () {
     //クリックしたときのファンクションをまとめて指定
-    $('.tab li').click(function () {
+    $('.Ptab li').click(function () {
 
         //.index()を使いクリックされたタブが何番目かを調べ、
         //indexという変数に代入します。
-        var index = $('.tab li').index(this);
+        var Pindex = $('.Ptab li').index(this);
 
         //コンテンツを一度すべて非表示にし、
-        $('.content li').css('display', 'none');
+        $('.Pcontent li').css('display', 'none');
 
         //クリックされたタブと同じ順番のコンテンツを表示します。
-        $('.content li').eq(index).css('display', 'block');
+        $('.Pcontent li').eq(Pindex).css('display', 'block');
 
         //一度タブについているクラスselectを消し、
-        $('.tab li').removeClass('select');
+        $('.Ptab li').removeClass('select');
 
         //クリックされたタブのみにクラスselectをつけます。
         $(this).addClass('select')
