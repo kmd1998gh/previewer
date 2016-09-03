827e0aea0d9a5474ee7c51cf4e88c66c15d6b808
kmd1998gh <kmd1998.wr@gmail.com>
2016-09-03 23:42:49 +0900

    Update preview.js

--- preview.js
+++ preview.js
@@ -2,20 +2,25 @@ $(function () {
     //クリックしたときのファンクションをまとめて指定
     $('.Ptab li').click(function () {
 
-        //.index()を使いクリックされたタブが何番目かを調べ、
-        //indexという変数に代入します。
-        var Pindex = $('.Ptab li').index(this);
-
-        //コンテンツを一度すべて非表示にし、
-        $('.Pcontent li').css('display', 'none');
-
-        //クリックされたタブと同じ順番のコンテンツを表示します。
-        $('.Pcontent li').eq(Pindex).css('display', 'block');
-
-        //一度タブについているクラスselectを消し、
-        $('.Ptab li').removeClass('select');
-
-        //クリックされたタブのみにクラスselectをつけます。
-        $(this).addClass('select')
+        //.index()を使いクリックされた親要素の子のタブが何番目かを変数indexに返す。
+        var index = $(this).parent().children().index(this);
+        //クリックされた値の親(previewer)の子(PContent)のさらに子(li)範囲をContentに代入する。css
+        var Content =$(this).parents('.previewer').children('.Pcontent').children('li');
+        
+        //クリックされた値の親(previewer)の子(Ptab)のさらに子(li)範囲をTabに代入する。
+        var Tab = $(this).parents('.previewer').children('.Ptab').children('li');
+        
+        
+        //Contentの範囲をすべて非表示にする
+        Content.css('display','none');
+        
+        //Contentの範囲でindex番目を表示する
+        Content.eq(index).css('display','block');
+        
+        //Tabの範囲にあるselectを削除
+        Tab.removeClass('select');
+        
+        //選択したタブにselectをつける
+        $(this).addClass('select');
     });
 });
